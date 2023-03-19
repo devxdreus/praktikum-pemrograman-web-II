e3036c2aa4d8c5f5735a384c6bdfe237533ed64a
Andri Firdaus <andricrafter007@gmail.com>
2023-03-19 23:08:51 +0800

    revisi modul 1

--- Modul 1/PRAK103.php
+++ Modul 1/PRAK103.php
@@ -2,10 +2,10 @@
 
 $celcius = 37.841;
 
-$fahrenheit = (9/5) * $celcius + 32;
-$reamur = (4/5) * $celcius;
+$fahrenheit = (9 / 5) * $celcius + 32;
+$reamur = (4 / 5) * $celcius;
 $kelvin = $celcius + 273.15;
 
-echo "$fahrenheit<br>";
-echo "$reamur<br>";
-echo "$kelvin";
+echo "Fahrenheit (F) : $fahrenheit<br>";
+echo "Reamur (R) : $reamur<br>";
+echo "Kelvin (K) : $kelvin";
